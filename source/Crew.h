69908a11e6b4693046ddc20b16f02da61420be25
Luke Arndt <lukearndt@users.noreply.github.com>
2019-11-30 15:48:45 +1000

    Add crew data type to enhance salary calculation (#10)
    This is a port of endless-sky/endless-sky/pull/4670
    at endless-sky/endless-sky@056a463
    
    ## Feature Details
    
    We can now calculate crew salaries using more factors than just how many
    crew members the player has in their active ships. This will allow us to
    create various different kinds of crew, each paid different amounts.
    
    This goal of this system is to allow for interesting trade-offs in fleet
    design. It also lets us create anti-snowballing mechanics, which
    help prevent the player from suddenly catapulting upward in power.
    
    Previously, salaries were constrained linearly: 100 credits per crew
    member across the entire fleet. Since income potential grows at a
    non-linear rate, linearly-scaling salaries soon become irrelevant.
    
    Using this tool, we can make salaries increase in proportion to actual
    power gain. This should hopefully add interesting decisions for the
    player - do they really need all those heavy warships, or will they cost
    too much to maintain?
    
    ### Context:
    
    Fleet management is currently very simple from an economic perspective.
    Crew members cost a flat 100 credits per day, regardless of what they
    actually do on their ship. Salary costs can be mostly ignored because
    the ships' benefits to the player far outweigh them.
    
    ### Changes:
    
    This commit makes crew salaries more complex by introducing the ability
    for us to define various kinds of crew members in the data files. The
    system currently supports the following attributes, with default values:
    
    - name
    - "avoids escorts" (false)
    - "avoids flagship" (false)
    - "parked salary" (0)
    - "place at" (empty list)
    - salary (100)
    - "ship population per member" (0)
    
    The commit includes a `crew.txt` data file with a few commented-out
    examples, plus an active data node for the default crew members.
    
    Since we also display the salaries on the Hiring panel, this commit
    changes that code to call the appropriate functions on the `Crew` class.
    
    Merging this PR will not meaningfully affect gameplay. It just gives us
    some tools that we can use for later changes.
    
    ### Considerations:
    
    I considered adding this logic to the `PlayerInfo` class, since that is
    where crew salaries used to be calculated. However, `PlayerInfo` is a
    really huge class with a ton of responsibilities. When editing it, I
    often got lost if I moved my editor window too far. Instead, I added a
    new class called `Crew`.
    
    Ideally it would be nice to update the fleet panel so that players can
    see how many of each crew member on a given ship and how much salary
    they are paid each day. This would help with decision making. We don't
    need to do that yet, though.

---
+++ source/Crew.h
@@ -0,0 +1,71 @@
+/* Crew.h
+Copyright (c) 2019 by Luke Arndt
+
+Endless Sky is free software: you can redistribute it and/or modify it under the
+terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later version.
+
+Endless Sky is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+*/
+
+#ifndef CREW_H_
+#define CREW_H_
+
+#include "Ship.h"
+
+class Crew
+{
+public:
+	// Calculate one day's salaries for the Player's fleet
+	static int64_t CalculateSalaries(const std::vector<std::shared_ptr<Ship>> &ships, const Ship * flagship, const bool includeExtras = true);
+	
+	// Calculate the total cost of the flagship's extra crew
+	static int64_t CostOfExtraCrew(const std::vector<std::shared_ptr<Ship>> &ships, const Ship * flagship);
+
+	// Figure out how many of a given crew member are on a ship
+	static int64_t NumberOnShip(const Crew &crew, const std::shared_ptr<Ship> &ship, const bool isFlagship, const bool includeExtras = true);
+
+	// Calculate one day's salaries for a ship
+	static int64_t SalariesForShip(const std::shared_ptr<Ship> &ship, const bool isFlagship, const bool includeExtras = true);
+
+	// List the crew members on a ship, and how many there are of each type
+	static const std::map<const std::string, int64_t> ShipManifest(const std::shared_ptr<Ship> &ship, bool isFlagship, bool includeExtras = true);
+
+	// Load a definition for a crew member.
+	void Load(const DataNode &node);
+	
+	bool AvoidsEscorts() const;
+	bool AvoidsFlagship() const;
+	int64_t ParkedSalary() const;
+	int64_t Salary() const;
+	int64_t ShipPopulationPerMember() const;
+	const std::string &Id() const;
+	const std::string &Name() const;
+	const std::vector<int64_t> &PlaceAt() const;
+
+private:
+	// If true, the crew member will not appear on escorts
+	bool avoidsEscorts = false;
+	// If true, the crew member will not appear on the flagship
+	bool avoidsFlagship = false;
+	// The number of credits paid daily while parked (minimum 0)
+	int64_t parkedSalary = 0;
+	// The number of credits paid daily (minimum 0)
+	int64_t salary = 100;
+	// Every nth crew member on the ship will be this crew member
+	int64_t shipPopulationPerMember = 0;
+	// The id that the crew member is stored against in GameData::Crews()
+	std::string id;
+	// The display name for this kind of crew members (plural, Title Case)
+	std::string name;
+	// The crew member will be placed at these crew member numbers if possible
+	// Note: if multiple crew definitions claim the same crew positions,
+	// we can end up paying for more crew than we expect to.
+	// To avoid this, don't place different crew members in the same spots.
+	// Example usage: "place at" 1 3 5 7 13
+	std::vector<int64_t> placeAt;
+};
+
+#endif
