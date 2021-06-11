d03a902266708c4fa4fac78bc29180e530e3eeb1
ravenshining <shining.raven@live.com>
2021-06-10 16:10:46 -1000

    Revert "Tidy up messaging"
    This reverts commit 8485412914a7da8eca5d93d38de7ec0c69342f9b.

--- source/BoardingPanel.cpp
+++ source/BoardingPanel.cpp
@@ -237,7 +237,7 @@ bool BoardingPanel::KeyDown(SDL_Keycode key, Uint16 mod, const Command &command,
 			Messages::Add(
 				"You must pay " +
 				Format::Number(profitShares) +
-				" credits to your crew as profit shares."
+				" credits for the profit shares of this boarding action to your fleet."
 			);
 			player.Accounts().AddProfitShares(profitShares);
 		}
